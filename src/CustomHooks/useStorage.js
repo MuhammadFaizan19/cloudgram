68022d7afc8d5e4d02b49986edd0b2c131da393a
MuhammadFaizan19 <faizanvkey@gmail.com>
2021-02-03 23:06:50 +0500

    custom hooks

---
+++ src/CustomHooks/useStorage.js
@@ -0,0 +1,32 @@
+import { useEffect, useState } from 'react';
+import {projectStorage,projectFirestore,timeStamp} from '../Firebase/Config';
+
+
+const useStorage=(file,firestore)=>{
+    const [progress, setProgress]=useState(0);
+    const [error,setError]=useState(null);
+    const [url, setUrl]=useState(null);
+
+    useEffect(()=>{
+
+        const storageRef=projectStorage.ref(file.name);
+        const collectionRef=firestore.collection('images');
+
+        storageRef.put(file).on('state_changed',(snap)=>{
+            let percentage=(snap.bytesTransferred/snap.totalBytes)*100;
+            setProgress(percentage);
+        },(err)=>{
+            setError(err);
+        }, async ()=>{
+            const url=await storageRef.getDownloadURL();
+            const createdAt=timeStamp();
+            collectionRef.add({url,createdAt})
+            setUrl(url);
+        })
+    },[file]);
+
+    return {progress,url,error};
+}
+
+
+export default useStorage;
