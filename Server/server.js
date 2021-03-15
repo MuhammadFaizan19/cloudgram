22b46ad5a66cf2c60a26f16070077e191c8a2c54
MuhammadFaizan19 <faizanvkey@gmail.com>
2021-03-15 13:53:42 +0500

    server req

--- Server/server.js
+++ Server/server.js
@@ -29,11 +29,11 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }))
 
 app.use('/api/images', routes);
-app.use(express.static((__dirname, 'build')))
+// app.use(express.static((__dirname, 'build')))
 
-app.get('/*', (req, res) => {
-  res.sendFile((__dirname + '/build/index.html'))
-})
+// app.get('/*', (req, res) => {
+//   res.sendFile((__dirname + '/build/index.html'))
+// })
 
 
 
